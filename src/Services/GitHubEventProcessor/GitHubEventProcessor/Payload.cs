5060de51fd1ddc4cb6d3a75e7506fd9480639c30
oneCodeSlinger <krishkotamarti@gmail.com>
2017-07-24 20:50:44 -0700

    GitHubsEventProcessor initial checkin
    Initial checkin of GitHubsEventProcessor with deserialization of all
    user events data. Next step is to map to Halo facets and hook into Azure
    WebJobs

---
+++ src/Services/GitHubEventProcessor/GitHubEventProcessor/Payload.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubpulluserevents
+{
+	class Payload
+	{
+		public int push_id { get; set; }
+		public int size { get; set; }
+		public int distinct_size { get; set; }
+		[JsonProperty(PropertyName = "ref")]
+		public string refs { get; set; }
+		public string head { get; set; }
+		public string before { get; set; }
+		public IList<Commit> Commits { get; set; }
+	}
+}
---
+++ src/Services/GitHubEventProcessor/GitHubEventProcessor/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubpulluserevents
+{
+	class UserData
+	{
+		public IList<Event> Events { get; set; }
+	}
+}
