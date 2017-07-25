36448c3360634d2259998d5f58a502c07b38b71a
oneCodeSlinger <krishkotamarti@gmail.com>
2017-07-24 21:42:07 -0700

    Revert "Revert "GitHubsEventProcessor initial checkin""
    This reverts commit d1c73322d72c55d4d4941db55ed9e4015cba3006.

---
+++ src/Services/GitHubEventProcessor/GitHubEventProcessor/Event.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubpulluserevents
+{	enum EventType
+	{
+		CreateEvent,
+		PushEvent,
+		PullRequestEvent,
+	}
+
+	class Event
+	{
+		public string id { get; set; }
+		public EventType type { get; set; }
+		public Organization actor { get; set; }
+		[JsonProperty(PropertyName = "repro")]
+		public Repository repository { get; set; }
+		public Payload payload { get; set; }
+
+		[JsonProperty(PropertyName = "public")]
+		public bool isPublic { get; set; }
+		public string created_at { get; set; }
+		[JsonProperty(PropertyName = "org")]
+		public Organization organization { get; set; }
+
+	}
+}
