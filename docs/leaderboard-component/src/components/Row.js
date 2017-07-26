e31fde23aa243000e00d58d23b8740bad1c5a816
Sean Sassenrath <sean.sassenrath@xamarin.com>
2017-07-26 15:47:29 -0700

    New build - More style tweaks

--- docs/leaderboard-component/src/components/Row.js
+++ docs/leaderboard-component/src/components/Row.js
@@ -16,16 +16,16 @@ export class Row extends Component {
 
     return (
       <div style={style}>
-        <div className="de-leaderboard--row">
+        <div className='de-leaderboard--row'>
           <div className='de-leaderboard--user-info-container'>
             <p className='de-leaderboard--p'>{`${index + 1}.`}</p>
             <img src={users[index].avatar_url} alt={users[index].login} className="de-leaderboard--avatar" />
             <p className='de-leaderboard--p'>{users[index].login}</p>
           </div>
           <div className='de-leaderboard--commits-container'>
-            <div>
-              <p className='de-leaderboard--p de-leaderboard--p-medium'>{`Points: ${points}XP`}</p>
-              <p className='de-leaderboard--p de-leaderboard--p-small'>{`Lines of code: ${commits}`}</p>
+            <div style={{textAlign: 'left'}}>
+              <p className='de-leaderboard--p-medium'>{`Points: ${points}XP`}</p>
+              <p className='de-leaderboard--p-small'>{`Lines of code: ${commits}`}</p>
             </div>
             <img src="https://user-images.githubusercontent.com/10020218/28635322-0791ebe0-71ef-11e7-8aa3-85eadefdcfed.png" alt="Halo" className="de-leaderboard--halo" />
           </div>
