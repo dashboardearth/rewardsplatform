61cd7067c76fd90a593a38d85ea396bb92f08f6d
Emily Luu <t-emluu@microsoft.com>
2017-07-25 17:24:00 -0700

    Dynamically adding image to circles, update city view indiv halo doc

--- src/city halo viz/data.js
+++ src/city halo viz/data.js
@@ -1,77 +1,77 @@
 var users = {
     "stephen": [
-        34.016611,-118.494626
+        34.016611,-118.494626,"http://www.terahard.org/assets/images/art/work07.jpg"
     ],
     "bigdatakane": [
-        34.018616,-118.506986
+        34.018616,-118.506986,"http://www.terahard.org/assets/images/art/work07.jpg"
     ],
     "mpell": [
-        34.021648,-118.502823
+        34.021648,-118.502823,"http://www.terahard.org/assets/images/art/work07.jpg"
     ],
     "hollybeale": [
-        34.009914,-118.495956
+        34.009914,-118.495956,"http://www.terahard.org/assets/images/art/work07.jpg"
     ],
     "aisha": [
-        34.011466,-118.501063
+        34.011466,-118.501063,"http://www.terahard.org/assets/images/art/work07.jpg"
     ],
     "brianirl": [
-        34.004929,-118.479219
+        34.021482, -118.488394,"http://s3files.core77.com/blog/images/557455_73100_59709_bztPFNFPR.jpg"
     ],
     "etluu": [
-        34.023467, -118.512092
+        34.023467, -118.512092,"http://s3files.core77.com/blog/images/557455_73100_59709_bztPFNFPR.jpg"
     ],
     "juliedeng": [
-        34.022778, -118.496557
+        34.022778, -118.496557,"http://s3files.core77.com/blog/images/557455_73100_59709_bztPFNFPR.jpg"
     ],
     "parker": [
-        34.028139, -118.468190
+        34.028139, -118.468190,"http://s3files.core77.com/blog/images/557455_73100_59709_bztPFNFPR.jpg"
     ],
     "rakotama": [
-        34.023859, -118.501740
+        34.023859, -118.501740,"http://s3files.core77.com/blog/images/557455_73100_59709_bztPFNFPR.jpg"
     ],
     "sasuppal": [
-        34.019391, -118.475047
+        34.019391, -118.475047,"http://atlasofthefuture.org/wp-content/uploads/2015/09/ORA_HALO_still3-1920x1477.jpg"
     ],
     "vinay": [
-        34.017614, -118.470465
+        34.017614, -118.470465,"http://atlasofthefuture.org/wp-content/uploads/2015/09/ORA_HALO_still3-1920x1477.jpg"
     ],
     "catiew": [
-        34.019992, -118.494392
+        34.019992, -118.494392,"http://atlasofthefuture.org/wp-content/uploads/2015/09/ORA_HALO_still3-1920x1477.jpg"
     ],
     "danasmirin": [
-        34.010364, -118.492685
+        34.010364, -118.492685,"http://atlasofthefuture.org/wp-content/uploads/2015/09/ORA_HALO_still3-1920x1477.jpg"
     ],
     "karinbauer108": [
-        34.035228, -118.478996
+        34.035228, -118.478996,"http://atlasofthefuture.org/wp-content/uploads/2015/09/ORA_HALO_still3-1920x1477.jpg"
     ],
     "joris": [
-        34.033014, -118.481474
+        34.033014, -118.481474,"https://crunchbase-production-res.cloudinary.com/image/upload/c_limit,h_600,w_600/v1427775361/mzyzvki6tfpbmman0dqv.jpg"
     ],
     "gaya": [
-        34.032458, -118.478127
+        34.032458, -118.478127,"https://crunchbase-production-res.cloudinary.com/image/upload/c_limit,h_600,w_600/v1427775361/mzyzvki6tfpbmman0dqv.jpg"
     ],
     "petercrnokrak": [
-        34.020364, -118.480583
+        34.020364, -118.480583,"https://crunchbase-production-res.cloudinary.com/image/upload/c_limit,h_600,w_600/v1427775361/mzyzvki6tfpbmman0dqv.jpg"
     ],
     "tbrodahl": [
-        34.032591, -118.494306
+        34.032591, -118.494306,"https://crunchbase-production-res.cloudinary.com/image/upload/c_limit,h_600,w_600/v1427775361/mzyzvki6tfpbmman0dqv.jpg"
     ],
     "ysunkara": [
-        34.027243, -118.503704
+        34.027243, -118.503704,"https://crunchbase-production-res.cloudinary.com/image/upload/c_limit,h_600,w_600/v1427775361/mzyzvki6tfpbmman0dqv.jpg"
     ],
     "zacharyostroff": [
-        34.007790, -118.476582
+        34.028867, -118.490497,"https://s-media-cache-ak0.pinimg.com/originals/14/13/53/14135321c72ae56078bced1bc8728da8.jpg"
     ],
     "tachi": [
-        34.013673, -118.494671
+        34.013673, -118.494671,"https://s-media-cache-ak0.pinimg.com/originals/14/13/53/14135321c72ae56078bced1bc8728da8.jpg"
     ],
     "laura": [
-        34.014647, -118.499208
+        34.014647, -118.499208,"https://s-media-cache-ak0.pinimg.com/originals/14/13/53/14135321c72ae56078bced1bc8728da8.jpg"
     ],
     "somesh.chandra": [
-        34.025500, -118.475519
+        34.025500, -118.475519, "https://s-media-cache-ak0.pinimg.com/originals/14/13/53/14135321c72ae56078bced1bc8728da8.jpg"
     ],
     "ritika": [
-        34.020774, -118.509079
+        34.020774, -118.509079, "https://s-media-cache-ak0.pinimg.com/originals/14/13/53/14135321c72ae56078bced1bc8728da8.jpg"
     ]
 }
